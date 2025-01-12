eb35720549214d28c0c987e958a3d6ff4c95c2ea
QiuYDvv <QiuYD2005@outlook.com>
2025-01-12 15:43:40 +0800

    reate the car's urdf which can be displayed in rviz and gazebo ,have loaded the controllers for four wheels,can through rostopic pub controller/left_front_wheel_controller/command  to push the car back and forth

---
+++ qyd_work/car_ws/src/robot_car/cmake-build-debug/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/qyd/Learning_task/qyd_work/car_ws/src/robot_car/cmake-build-debug/gtest/googletest
+#
+# This file includes the relevant testing commands required for
+# testing this directory and lists subdirectories to be tested as well.
