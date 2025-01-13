06923765b771e275dee516d63fd31390e8624d55
Whathelp233 <3351730846@qq.com>
2025-01-13 16:37:40 +0800

    Add car and its controller.

---
+++ learning_task/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/what/桌面/Learning_task/learning_task
+# Build directory: /home/what/桌面/Learning_task/learning_task/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
