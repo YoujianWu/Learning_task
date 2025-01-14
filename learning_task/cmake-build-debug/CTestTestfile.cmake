e21f915359ec2179a09056bd5f86bab1bc8b3192
Whathelp233 <3351730846@qq.com>
2025-01-14 18:32:09 +0800

    fixed gitignore and notes

--- learning_task/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/what/桌面/Learning_task/learning_task
-# Build directory: /home/what/桌面/Learning_task/learning_task/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- learning_task/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "learning_task"
-PROJECT_SPACE_DIR = "/home/what/桌面/Learning_task/learning_task/cmake-build-debug/devel"
-PROJECT_VERSION = "0.0.0"
